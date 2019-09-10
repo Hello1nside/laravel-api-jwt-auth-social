21f088d47d7164289f48d530573823ffdf9c9664
Yevhenii Mudrahel <mudrahel@gmail.com>
2019-09-10 18:06:27 +0300

    Initial commit

---
+++ app/Http/Controllers/api/Auth/LoginController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers\api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Tymon\JWTAuth\Exceptions\JWTException;
+use Tymon\JWTAuth\JWTAuth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '';
+
+    protected $auth;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle a login request to the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return response()->json([
+                'success' => false,
+                'errors' => [
+                    "You've been locked out"
+                ]
+            ]);
+        }
+
+        // If the login attempt was unsuccessful we will increment the number of attempts
+        // to login and redirect the user back to the login form. Of course, when this
+        // user surpasses their maximum number of attempts they will get locked out.
+        $this->incrementLoginAttempts($request);
+
+        // attempt login with token
+        if ($request->input('token')) {
+            $this->auth->setToken($request->input('token'));
+
+            $user = $this->auth->authenticate();
+            if ($user) {
+                return response()->json([
+                    'success' => true,
+                    'data' => $request->user(),
+                    'token' => $request->input('token')
+                ], 200);
+            }
+        }
+
+        try {
+            if (!$token = $this->auth->attempt($request->only('email', 'password'))) {
+                return response()->json([
+                    'success' => false,
+                    'errors' => [
+                        'email' => [
+                            "Invalid email address or password"
+                        ]
+                    ]
+                ], 422);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'success' => false,
+                'errors' => [
+                    'email' => [
+                        "Invalid email address or password"
+                    ]
+                ]
+            ], 422);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $request->user(),
+            'token' => $token
+        ], 200);
+    }
+}
---
+++ app/Http/Controllers/api/Auth/SocialLoginController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\api\Auth;
+
+use App\Models\User;
+use App\Models\UserSocial;
+use Laravel\Socialite\Facades\Socialite;
+use App\Http\Controllers\Controller;
+use Tymon\JWTAuth\JWTAuth;
+
+class SocialLoginController extends Controller
+{
+    protected $auth;
+
+    public function __construct(JWTAuth $auth)
+    {
+        $this->auth = $auth;
+        $this->middleware(['social', 'web']);
+    }
+
+    public function redirect($service)
+    {
+        return Socialite::driver($service)->redirect();
+    }
+
+    public function callback($service)
+    {
+        try {
+            $serviceUser = Socialite::driver($service)->user();
+        } catch (\Exception $e) {
+            return redirect(env('CLIENT_BASE_URL') . '/auth/social-callback?error=Unable to login using ' . $service . '. Please try again' . '&origin=login');
+        }
+
+        if ((env('RETRIEVE_UNVERIFIED_SOCIAL_EMAIL') == 0) && ($service != 'google')) {
+            $email = $serviceUser->getId() . '@' . $service . '.local';
+        } else {
+            $email = $serviceUser->getEmail();
+        }
+
+        $user = $this->getExistingUser($serviceUser, $email, $service);
+        $newUser = false;
+        if (!$user) {
+            $newUser = true;
+            $user = User::create([
+                'name' => $serviceUser->getName(),
+                'email' => $email,
+                'password' => ''
+            ]);
+        }
+
+        if ($this->needsToCreateSocial($user, $service)) {
+            UserSocial::create([
+                'user_id' => $user->id,
+                'social_id' => $serviceUser->getId(),
+                'service' => $service
+            ]);
+        }
+
+        return redirect(env('CLIENT_BASE_URL') . '/auth/social-callback?token=' . $this->auth->fromUser($user) . '&origin=' . ($newUser ? 'register' : 'login'));
+    }
+
+    public function needsToCreateSocial(User $user, $service)
+    {
+        return !$user->hasSocialLinked($service);
+    }
+
+    public function getExistingUser($serviceUser, $email, $service)
+    {
+        if ((env('RETRIEVE_UNVERIFIED_SOCIAL_EMAIL') == 0) && ($service != 'google')) {
+            $userSocial = UserSocial::where('social_id', $serviceUser->getId())->first();
+            return $userSocial ? $userSocial->user : null;
+        }
+        return User::where('email', $email)->orWhereHas('social', function($q) use ($serviceUser, $service) {
+            $q->where('social_id', $serviceUser->getId())->where('service', $service);
+        })->first();
+    }
+}
